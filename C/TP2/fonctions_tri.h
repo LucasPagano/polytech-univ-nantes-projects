04af6230d7b169bf19a462278ff4d763cf91f61a
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2017-02-16 16:19:59 +0100

    Plus propre TP2 C

--- C/TP2/fonctions_tri.h
+++ C/TP2/fonctions_tri.h
@@ -1,12 +1,12 @@
 #ifndef _FONCTIONS_TRI_H
   #define _FONCTIONS_TRI_H
 
-  typedef void (*algo_tri)(int* tableau,int longueur, int (*comp)(int, int)); // Definition des algorithmes de tri
   typedef int (*Comp)(int, int); // Definition de la fonction de comparaison
+  typedef void (*algo_tri)(int*, int, Comp); // Definition des algorithmes de tri
 
   void swap_tab(int *i, int *j); // Echange deux elements d'un tableau
 
-  int partitionner(int* tab, int premier, int dernier, int pivot, int (*comp)(int, int));
+  int partitionner(int* tab, int premier, int dernier, int pivot, Comp comp);
   void qsort_tab(int* tab, int premier, int dernier, int (*comp)(int, int));
   void tri_rapide(int* tableau,int longueur, Comp comp); // Sert simplement a avoir la meme signature pour qsort_tab que les autres
 
