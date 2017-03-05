bc26928b2946afe9db0e17356db216ff7912595c
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2017-03-05 14:39:14 +0100

    exam 2015

--- Prolog/stir.pl
+++ Prolog/stir.pl
@@ -1,7 +1,13 @@
-st(N,N,1).
-st(_,1,1).
-st(N,K,R):- K < N, K > 1,
-	N1 is N-1, K1 is K-1,
-	st(N1,K1,R1),
-	st(N1,K,R2),
-	R is R1 + K*R2.
+stir(N,K,1):-
+  K =< N,
+  N>=1,
+  N=K;K=1.
+
+stir(N,K,R):-
+  K =< N,
+  N>=1,
+  N1 is N-1,
+  K1 is K-1,
+  stir(N1,K1,R1),
+  stir(N1,K,R2),
+  R is R1 + K*R2.
