28baa34978b2cd6657323359fdd5b4dad4837667
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2018-01-08 18:04:46 +0100

    added usage instructions

--- main.pl
+++ main.pl
@@ -1,3 +1,20 @@
 :- include('sortDimensions.pl').
 :- include('scoreGrille.pl').
 :- include('tests.pl').
+
+/*
+How to use :
+
+For best permutation problem :
+main(NbIndiv, NbDimensions, (Permutation, Area)).
+or : main(NbIndiv, NbDimensions, Result).
+example : main(4, 8,(Permutation, Area)).
+For few individuals and dimensions, can use mainWithBest(NbIndiv, NbDimensions, (Permutation, Area)). to have best solution
+
+For grid problem :
+main. gives a demonstration of what the program can do
+
+To run tests :
+run_tests.
+
+*/
